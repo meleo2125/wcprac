e77c9ccfa8f0f725ba23f051db3b798a078a6ff0
meleo <mukeshprajapat3093@gmail.com>
2023-11-04 19:53:04 +0530

    files added

--- welcome/src/App.js
+++ welcome/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
-class Welcome extends React.components {
+class Welcome extends React.Component {
     greetUser = () => {
         alert('Hello, Welcome!');
     };
     render(){
+        const buttonStyle = {
+            backgroundColor: '#00d991',
+            color: 'white',
+            padding: '10px',
+            borderRadius: '5px',
+            cursor: 'pointer',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)'
+        };
         return(
-            <button onClick = {this.greetUser}>Greet the User</button>
+            <button onClick = {this.greetUser} style={buttonStyle}>Greet the User</button>
         );
     }
 }
